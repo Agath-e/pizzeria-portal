e2253fc2998ab6e4bb5ab1c21604718a443077f3
Agata Pacholczyk <agataa.pacholczyk@gmail.com>
2020-07-16 19:57:36 +0200

    add components in views(Dashboard,Login,Kitchen,Weiter,Tables [18.2]

---
+++ VIEWS.md
@@ -0,0 +1,55 @@
+# Dashboard
+
+- '/'
+    - statystyki dzisiejszych zamówień (zdalne i lokalne)
+    - listę rezerwacji i eventów zaplanowanych na dzisiaj
+
+# Logowanie
+
+- '/login'
+    - pola na login i hasła
+    - guzik do zalogowania (link do dashboardu)
+
+# Widok dostępności stolików
+
+- '/tables'
+    - wybór daty i godziny
+    - tabela z listą rezerwacji oraz wydarzeń
+        - każda kolumna = 1 stolik
+        - każdy wiersz = 30 minut
+        - ma przypominać widok tygodnia Google, gdzie w kolumnach zamiast dni są różne stoliki
+        - po kliknieciu rezerwacji lub eventu, przechodzimy na stronę szczegółow
+- '/tables/booking/:id'
+    - musi ona zwierać wszsytkie informację dotyczące irezerwacji
+    - umożliwa edycję i zapisanie zmian
+- '/tables/booking/new'
+    - analogocznie do powyższej, bez początkowych informacji
+- '/tables/events/:id'
+    - analogocznie do powyższej,dla eventów
+- '/tables/events/new'
+    - analogocznie do powyższej, bez początkowych informacji
+
+# Widok kelnera
+
+- '/waiter'
+    - tabela
+        - w iwerszach stoliki
+        - w kolumnach różne rodzaje inforamcji (status, czas od ostatniej aktywności)
+        - w ostatniej kolumnie dostępne akcje dla danego stolika
+- '/waiter/order/new'
+    - numer stolika (edytowalny)
+    - menu produktów
+    - opje wybranego produktu
+    - zamówienie (zamówione produkty z opcjami i ceną)
+    - kwotę zamówienia
+- '/waiter/order/:id'
+    - jak powyższa
+
+# Widok kuchni
+
+-'/kitchen'
+    - wyświetla listę zamówień w koejności ich złożenia 
+    - lista musi zawierać:
+        - numer stolika (lub zamówienia zdalnego) 
+        - pełne informacje dot. zamówionych dań
+    na liście musi być mozliwość oznaczenia zamówienia jako zrealizowane
---
+++ public/front/js/app.js
@@ -0,0 +1,129 @@
+import {settings, select, classNames} from './settings.js';
+import Product from './components/Product.js';
+import Cart from './components/Cart.js';
+import {Booking} from './components/Booking.js';
+
+const app = {
+  initBooking: function() {
+    const thisApp = this;
+
+    const bookingWidgetElem = document.querySelector(select.containerOf.booking);
+    thisApp.bookingWidget = new Booking(bookingWidgetElem);
+
+  },
+  initPages: function() {
+    const thisApp = this;
+
+    thisApp.pages = Array.from(document.querySelector(select.containerOf.pages).children);
+    thisApp.navLinks = Array.from(document.querySelectorAll(select.nav.links));
+    thisApp.navBox = Array.from(document.querySelectorAll('.nav-box a'));
+
+    thisApp.activatePage(thisApp.pages[0].id);
+
+    let pagesMatchingHash = [];
+
+    if (window.location.hash.length > 2) { 
+      const idFromHash = window.location.hash.replace('#/', '');
+    
+      pagesMatchingHash = thisApp.pages.filter(function (page) {
+        return page.id == idFromHash;   
+      });
+
+      thisApp.activatePage(pagesMatchingHash.length ? pagesMatchingHash[0].id : thisApp.pages[0].id);
+    }
+
+    //for(let page of thisApp.pages){
+    //  if(page.id == idFromHash){
+    //    pageMatchingHash = page.id;
+    //    break;
+    //  }
+    //}  
+    //console.log('idFromHash', idFromHash);
+    //thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* get page id form href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', ''); 
+        /* run thisApp.activatePage with that id */
+        thisApp.activatePage(id);
+       
+      });
+    }
+    for(let link of thisApp.navBox){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* get page id form href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', ''); 
+        /* run thisApp.activatePage with that id */
+        thisApp.activatePage(id);
+       
+      });
+    } 
+  },
+  activatePage: function(pageId) {
+    const thisApp = this;
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+
+    /* change URL hash */
+    window.location.hash = '#/' + pageId;
+
+  },
+  initCart: function() {
+    const thisApp = this;
+    const cartElem = document.querySelector(select.containerOf.cart);
+    thisApp.cart = new Cart(cartElem);
+
+    thisApp.productList = document.querySelector(select.containerOf.menu);
+
+    thisApp.productList.addEventListener('add-to-cart', function(event) {
+      app.cart.add(event.detail.product);
+      
+    });
+  },
+  initData: function() {
+    const thisApp = this;
+
+    thisApp.data = {};
+    const url = settings.db.url + '/' + settings.db.product;
+
+    fetch(url)
+      .then(function(rawResponse){
+        return rawResponse.json();
+      })
+      .then(function(parsedResponse){
+        thisApp.data.products = parsedResponse;
+        app.initMenu();
+      });
+  },
+  initMenu: function() {
+    const thisApp = this;
+
+    for (let productData in thisApp.data.products) {
+      new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
+    }
+  },
+  init: function() {
+    const thisApp = this;
+
+    thisApp.initPages();
+    thisApp.initData();
+    thisApp.initCart();
+    thisApp.initBooking();
+  },
+};
+
+app.init();
